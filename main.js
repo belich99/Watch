bf29e21b1a546e2c89cd0174b465cbae83540343
belich99 <84977353+belich99@users.noreply.github.com>
2022-01-30 11:38:05 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.header2').click(function(){
+        $('body').hide(100);
+        
+    })
+})
+
+// $(function () {
+//     $('.collection_slider').slick({
+//         dots: true,
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         arrows: false,
+//         autoplay: true,
+//         mobileFirst: true,
+//         autoplaySpeed: 2000,
+//         speed: 1500,
+        
+
+//     })
+// })
+
